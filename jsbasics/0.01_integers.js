b1d0a29fde5de43942b28f22272656052c13c84f
LDecker <Decker702@yahoo.com>
2016-09-21 10:35:03 -0400

    Added JS Files and store app

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,76 @@
+// var num = 9; //all start out lower case.
+// var newNum = 3; //this is called camel casing.  Starts out lower case and hump in the middle. - FYI you don't need to close the comment.  Also only good for one line.
+// var newer_num = 10; //this is snake casing
+
+// console.log(num); //can console.log along the way to see the results of what you have done in the console (at the bottom of the page).  Consule.log prints to the bottom of the page.
+
+// var x = 5;
+// var y = "Paul";
+
+// var userName = "Zack";
+
+// console.log (userName)	
+
+
+// console.log(5+5); //Operations with console.log  Use to do - math, ie - addition, subtractiuon, multiplication and division.
+// console.log (10-5)
+// console.log (7*5)
+// console.log(200/40)
+
+// //Need to hit command B to get it to show in the console.  It shows all the work above.
+
+// //Modulus Operator - logic operator which is used a lot.  It is the math that helps build functionality
+// console.log(10 % 5); //returns zero because there is no remainder
+// console.log(4 % 3) // returns 1 because 4 divided by 3 has a remainder of 1.  
+// // division first and then spits back the remainder, which is what it "spits" back.
+
+// //TODO - make a modulus that returns zero and another that returns a reminder
+
+// console.log (200 % 50);
+// console.log (200 % 30);
+
+// //TODO - try to make a variable tht is assigned an operation and console log it.
+// var numOp = 25 + 100;
+// console.log(numOp)
+
+// //TODO - try to assign numbers to different variables and perform an 
+// //operation to them.
+// var a=5;
+// var b=10;
+// console.log (a * b);
+
+// //TODO - try to assigne numbers to two different variables then assign
+// //any operation on these two variables to get a new variables.
+// // variables allow us to use inputs
+// var c=a+b;
+// console.log(c);
+
+
+var newNum=10;
+newNum = newNum + 1; //this is a reassignment.  Original variable +1
+console.log (newNum);
+newNum++;  //short cut for NewNum = newNum + 1;
+console.log (newNum);
+
+newNum = newNum -1;
+console.log (newNum);
+newNum--;
+console.log (newNum);
+
+newNum = newNum + 10; //this is a reassignment.  Original variable +10
+console.log (newNum);
+newNum+=10;  //short cut for NewNum = newNum + 10.  This is the20 above + 10 more to get to 30.
+console.log (newNum);
+
+newNum = newNum -10;
+console.log (newNum);
+newNum-=10;
+console.log (newNum); // this line of math continues down the original variable down.
+
+newNum *=10;
+console.log (newNum); //this should be multiplication
+
+newNum /=10;
+console.log (NewNum); //this should be division
+
+
