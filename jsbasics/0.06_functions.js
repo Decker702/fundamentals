e0ddd3954dadda91d5e37fb13b3e40b8afbe5d85
LDecker <Decker702@yahoo.com>
2016-09-22 09:39:04 -0400

    Answers to function Challenge:

--- jsbasics/0.06_functions.js
+++ jsbasics/0.06_functions.js
@@ -94,7 +94,13 @@
 	// 	return a + b
 	// }
 	// console.log (bronze("go", "fish"))
- 
+  
+//Below is Paul's example on Thursday 9/22/16
+  function combineWords(wordOne, wordTwo){
+  		return wordOne+wordTwo;
+  };//this semi-colon is optional
+  console.log(combineWords("Diet ", "Coke"));// the space after Diet and before the end quote is what 
+  //gives you a space in the console print between diet & coke.
 
 //SILVER:
 // 	Write a function that will help me easily 
@@ -109,17 +115,33 @@
 // 	var house = 876;
 // console.log (gas + phone + electric + water + house);  
 // }
-// if the above worked, it would be hard coded, but the one below give you flexibility.
+// // James on Wed /21/17 did the below
+//if the above worked, it would be hard coded, but the one below give you flexibility.
 //You just have enter in the amount due in console.log and not in the function.
 
+//
 // function monthyBills(rent, phone, dateNight){
 // 	return rent + phone +dateNight;
 // }
-
 // console.log (monthyBills (200, 50, 100))
-// /*-----------------------------------------------------------
-// 
 
+//Below is Paul's example on Thursday 9/22/16
+function bills (rent, water, electric, trash, wireless){
+	var totalBills = rent + water + electric + trash + wireless;
+	return totalBills
+}
+
+console.log ("Bills:", bills(600, 30, 30, 10, 30));
+
+function bills (rent, water, electric, trash, wireless){
+	var totalBills = rent + water + electric + trash + wireless;
+	return totalBills
+}
+var paulBills = bills(400, 30, 30, 10, 30);
+var jamesBills = bills(600, 20, 10,10, 30);
+console.log("Jame's Bills:" + jamesBills); //Answered to question on individual bills.
+console.log("Paul's Bills:" + paulBills);
+console.log ("Bills:", bills(600, 30, 30, 10, 30));
 /*-----------------------------------------------------------
 GOLD:
 Write a function that helps me calculate the cost of my buying 100 sodas
@@ -132,13 +154,19 @@ Return a message that says something like this:
 //you need to identify - number bought, brand bought, cost is $0.99 each, add them together
 // then multipy the total by 1.07 to get the total.  You only care about the brand in the message.
 
-function purchase (cans, cost, tax){
-	return (cans * cost * tax);
+// function purchase (cans, cost, tax){
+// 	return (cans * cost * tax);
+// }
+
+// console.log (purchase (100, .99, 1.07))
+
+//Below is Paul's example on Thursday, 9/22/16
+function calculatePrice(quanity, price, item, tax){
+
+	var totalTax = (quanity * price * tax);
+	var totalPrice = quanity * price + totalTax;
+	return quantity + " " +item + " will cost you $" + totalPrice.toFixed(2); //this keeps it at 2 number after decimal
+
+console.log (calculatePrice (132, .88, "Cups of Coffee", 1.07));
+
 }
-
-console.log (purchase (100, .99, 1.07))
-
-function message (){
-	console.log (cans "of Coke cost" purchase):
-}
-
